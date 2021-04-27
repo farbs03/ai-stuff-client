e282fa6efd943429287b6d6ba1e66bc793b9b23b
farbs03 <cgeraldfarber@icloud.com>
2021-04-26 22:10:42 -0400

    better formatting

--- src/Components/Summarizer/Summarizer.jsx
+++ src/Components/Summarizer/Summarizer.jsx
@@ -10,7 +10,7 @@ const Summarizer = () => {
         setResult(null)
         const formData = link;
         setLoading(true)
-        fetch('http://127.0.0.1:5000/summarize', 
+        fetch('https://ai-stuff-backend.herokuapp.com/summarize', 
           {
             headers: {
               'Accept': 'application/json',
