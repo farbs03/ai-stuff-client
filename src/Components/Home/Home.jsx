82e94e66d7f6916b8bf6b280437880712424448e
farbs03 <cgeraldfarber@icloud.com>
2021-05-04 21:38:59 -0400

    explanation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <p>Often times, there are cool ai projects out there, but not so often, they aren't interactive. I hope to make ai a bit more fun through this website, to tangibly show what's possible with it.</p>
         </div>
     )
 }
